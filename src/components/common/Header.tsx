9d212e1e3023e804fbce703e2a27f2292688b7dd
Kwonyeojiny <kangagi0009@naver.com>
2025-04-02 01:34:13 +0900

    [Feat] nav 메뉴 호버시 애니메이션 구현

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -10,18 +10,23 @@ const Header = () => {
         <nav className="flex flex-row gap-16 text-base">
           <Link href="/map" className="group relative">
             지도
+            <span className="absolute -bottom-1 left-1/2 transform -translate-x-1/2 w-0 h-1 bg-red group-hover:w-full transition-all duration-300"></span>
           </Link>
           <Link href="/nightViewSpot" className="group relative">
             야경명소
+            <span className="absolute -bottom-1 left-1/2 transform -translate-x-1/2 w-0 h-1 bg-orange group-hover:w-full transition-all duration-300"></span>
           </Link>
           <Link href="/market" className="group relative">
             전통시장
+            <span className="absolute -bottom-1 left-1/2 transform -translate-x-1/2 w-0 h-1 bg-yellow group-hover:w-full transition-all duration-300"></span>
           </Link>
           <Link href="/historicSite" className="group relative">
             유적지
+            <span className="absolute -bottom-1 left-1/2 transform -translate-x-1/2 w-0 h-1 bg-green group-hover:w-full transition-all duration-300"></span>
           </Link>
           <Link href="/menu" className="group relative">
             메뉴
+            <span className="absolute -bottom-1 left-1/2 transform -translate-x-1/2 w-0 h-1 bg-blue group-hover:w-full transition-all duration-300"></span>
           </Link>
         </nav>
         <div className="flex gap-2 text-base">
