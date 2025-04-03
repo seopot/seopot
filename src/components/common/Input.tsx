58757bf4016d4d6baa89db2dbbb63fb19a1ce220
Ju-MINJAE <juminjae98@gmail.com>
2025-04-03 17:13:35 +0900

    [Design] adjust responsive layout

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -12,18 +12,18 @@ const Input = () => {
   };
 
   return (
-    <div className="w-[80%] max-w-[60rem] md:max-w-[40rem] flex">
+    <div className="w-[80%] max-w-[60rem] sm:max-w-[40rem] flex">
       <input
         type="text"
         ref={inputRef}
         placeholder="검색어를 입력하세요."
-        className="w-full px-4 py-2 bg-lightBeige rounded-l-2xl focus:outline-none sm:text-sm dark:text-navy"
+        className="w-full px-4 py-2 bg-lightBeige rounded-l-2xl focus:outline-none text-sm sm:text-base dark:text-navy"
       />
       <button
         onClick={handleSearch}
-        className="flex justify-center items-center px-4 py-2 bg-lightBeige rounded-r-2xl hover:bg-lightNavy sm:px-3 dark:text-navy"
+        className="flex justify-center items-center px-3 py-2 bg-lightBeige rounded-r-2xl hover:bg-lightBrown sm:px-4 dark:text-navy"
       >
-        <Search className="w-5 h-5 sm:w-4 sm:h-4" />
+        <Search className="w-4 h-4 sm:w-5 sm:h-5" />
       </button>
     </div>
   );
