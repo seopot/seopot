f929c230db498b3b21644cdbb1846ac6f00165b2
Ju-MINJAE <juminjae98@gmail.com>
2025-04-03 18:12:03 +0900

    [Design] add responsive design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
 import Input from '@/components/common/Input';
+import ItemCard from '@/components/common/ItemCard';
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex flex-col justify-center items-center w-full">
       <Input />
+      <div className="w-full flex flex-col sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4 md:gap-[2.125rem] px-4 md:px-[8.375rem] mt-8">
+        <ItemCard />
+        <ItemCard />
+        <ItemCard />
+        <ItemCard />
+        <ItemCard />
+        <ItemCard />
+        <ItemCard />
+      </div>
     </div>
   );
 }
--- src/components/common/ItemCard.tsx
+++ src/components/common/ItemCard.tsx
@@ -1,13 +1,23 @@
 import Image from 'next/image';
 
+const TEMPORARY_WORD = '대한민국';
+
 const ItemCard = () => {
-  // Todo: 이미지 src를 props로 받아오도록 변경하기
+  // Todo: 이미지 src, text를 props로 받아오도록 변경하기
   // 현재 임시 이미지 사용
 
   return (
-    <div className="relative w-[18.75rem] h-[17.8125rem] bg-lightBeige">
-      <div className="absolute inset-0 flex items-center justify-center m-auto w-[17.5rem] h-[16.25rem] bg-white ">
-        <Image src="/images/n.png" alt="temporary_img" fill className="object-cover" />
+    <div className="relative w-full sm:w-[18.75rem] h-[6.875rem] sm:h-[17.8125rem] bg-lightBeige group">
+      <div className="absolute inset-0 flex items-center justify-center m-auto w-[95%] md:w-[17.5rem] h-[90%] md:h-[16.25rem] bg-lightBeige overflow-hidden">
+        <Image
+          src="/images/n.png"
+          alt="temporary_img"
+          fill
+          className="object-cover sm:transition-all sm:duration-300 sm:group-hover:brightness-50"
+        />
+        <span className="absolute bottom-0 sm:bottom-4 right-4 text-lightBeige sm:opacity-0 sm:group-hover:opacity-100 sm:transition-opacity sm:duration-300 text-[1.6875rem] font-bold">
+          {TEMPORARY_WORD}
+        </span>
       </div>
     </div>
   );
