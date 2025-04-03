83902808e2d6dca98ec28aae36877a08bd8cda02
Ju-MINJAE <juminjae98@gmail.com>
2025-04-03 17:39:54 +0900

    [Comment] add todo Comment

--- src/components/common/ItemCard.tsx
+++ src/components/common/ItemCard.tsx
@@ -1,6 +1,9 @@
 import Image from 'next/image';
 
 const ItemCard = () => {
+  // Todo: 이미지 src를 props로 받아오도록 변경하기
+  // 현재 임시 이미지 사용
+
   return (
     <div className="relative w-[18.75rem] h-[17.8125rem] bg-lightBeige">
       <div className="absolute inset-0 flex items-center justify-center m-auto w-[17.5rem] h-[16.25rem] bg-white ">
