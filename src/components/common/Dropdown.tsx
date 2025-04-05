d55803548bfeb10879ced31f59d8b02779d76536
Ju-MINJAE <juminjae98@gmail.com>
2025-04-05 15:15:18 +0900

    [Design] add z-index & modify position

--- src/components/common/Dropdown.tsx
+++ src/components/common/Dropdown.tsx
@@ -38,13 +38,13 @@ const Dropdown: React.FC<DropdownProps> = ({ options, defaultOption = '한국어
   };
 
   return (
-    <article className="relative" ref={dropdownRef}>
+    <article className="relative z-30" ref={dropdownRef}>
       <button onClick={() => setIsOpen(!isOpen)} className="flex items-center gap-2">
         {selectedOption}
       </button>
 
       {isOpen && (
-        <ul className="absolute top-full left-0 mt-1 bg-lightBeige text-navy py-1 min-w-[6rem]">
+        <ul className="absolute top-full -left-4 lg:-left-8 mt-1 bg-lightBeige text-navy py-1 min-w-[6rem]">
           {options.map(option => (
             <li key={option}>
               <button onClick={() => handleSelect(option)} className="w-full px-4 py-2 text-left">
