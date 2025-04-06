7b87174e6e905b131e7fa613c1f6d4b104ec8043
myeong-jae-hwi <audwognl@gmail.com>
2025-04-06 15:57:25 +0900

    [Comment] 참조 링크 변경

--- src/components/map/KakaoLoader.tsx
+++ src/components/map/KakaoLoader.tsx
@@ -3,7 +3,7 @@ import { useKakaoLoader as useKakaoLoaderOrigin } from 'react-kakao-maps-sdk';
 /*
  *
  * 해당 코드는 아래 가이드 링크를 참고했습니다!
- * https://apis.map.kakao.com/web/guide/
+ * https://react-kakao-maps-sdk.jaeseokim.dev/
  *
  */
 
