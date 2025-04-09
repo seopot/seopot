d9e2586745f1edfef45e13ca63768e04166c1cf1
Ju-MINJAE <juminjae98@gmail.com>
2025-04-09 22:30:13 +0900

    [Refactor] seperate HTTP Client logic

--- src/app/nightViewSpot/page.tsx
+++ src/app/nightViewSpot/page.tsx
@@ -1,23 +1,20 @@
 'use client';
 
 import { useEffect } from 'react';
+import { getNightViewSpots } from '@/services/getNightViewSpots';
 
 const NightViewSpot = () => {
   useEffect(() => {
-    const fetchNightViewSpots = async () => {
+    const fetchData = async () => {
       try {
-        const start = 1;
-        const end = 51;
-        const apiUrl = `${process.env.NEXT_PUBLIC_NIGHT_VIEW_SPOT_URL}/${start}/${end}`;
-        const res = await fetch(apiUrl);
-        const data = await res.json();
+        const data = await getNightViewSpots(1, 51);
         console.log('야경 명소 데이터:', data);
       } catch (error) {
-        console.error('데이터 가져오기 실패:', error);
+        console.error('가져오기 실패:', error);
       }
     };
 
-    fetchNightViewSpots();
+    fetchData();
   }, []);
 
   return <div>Night View Spot</div>;
