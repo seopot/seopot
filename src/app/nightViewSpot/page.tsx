998f5474aa48faeb98dbd59805da360b3dde5277
Ju-MINJAE <juminjae98@gmail.com>
2025-04-09 22:18:54 +0900

    [Feat] OpenAPI data connect test

--- src/app/nightViewSpot/page.tsx
+++ src/app/nightViewSpot/page.tsx
@@ -2,10 +2,25 @@
 
 import { useEffect } from 'react';
 
-const nightViewSpot = () => {
-  useEffect(() => {}, []);
+const NightViewSpot = () => {
+  useEffect(() => {
+    const fetchNightViewSpots = async () => {
+      try {
+        const start = 1;
+        const end = 51;
+        const apiUrl = `${process.env.NEXT_PUBLIC_NIGHT_VIEW_SPOT_URL}/${start}/${end}`;
+        const res = await fetch(apiUrl);
+        const data = await res.json();
+        console.log('야경 명소 데이터:', data);
+      } catch (error) {
+        console.error('데이터 가져오기 실패:', error);
+      }
+    };
 
-  return <div>nightViewSpot</div>;
+    fetchNightViewSpots();
+  }, []);
+
+  return <div>Night View Spot</div>;
 };
 
-export default nightViewSpot;
+export default NightViewSpot;
