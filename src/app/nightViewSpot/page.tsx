3a4a7bc986c421932c2a8ae7f542b17c72a10fb2
Ju-MINJAE <juminjae98@gmail.com>
2025-04-09 22:15:02 +0900

    [Feat] create nightViewSpot page

---
+++ src/app/nightViewSpot/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { useEffect } from 'react';
+
+const nightViewSpot = () => {
+  useEffect(() => {}, []);
+
+  return <div>nightViewSpot</div>;
+};
+
+export default nightViewSpot;
