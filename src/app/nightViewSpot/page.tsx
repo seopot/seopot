b5386858653933f5ccbc643890d66e223ee69fd5
Ju-MINJAE <juminjae98@gmail.com>
2025-04-14 21:34:02 +0900

    [Fix]  use unique key instead of index in map

--- src/app/nightViewSpot/page.tsx
+++ src/app/nightViewSpot/page.tsx
@@ -16,8 +16,6 @@ const NightViewSpot = () => {
         const data = await getNightViewSpots(1, 51);
         // console.log(data.viewNightSpot.row);
         setSpots(data.viewNightSpot.row);
-
-        console.log('야경 명소 데이터:', spots);
       } catch (error) {
         console.error('가져오기 실패:', error);
         setError('야경 명소 데이터를 불러오는데 실패했습니다.');
@@ -29,19 +27,15 @@ const NightViewSpot = () => {
     fetchData();
   }, []);
 
+  // loading이랑 error는 임시로 사용
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>에러 : {error}</div>;
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <h1 className="text-2xl font-bold mb-6">야경 명소</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {spots.map((spot, index) => (
-          <ItemCard
-            key={index}
-            imgSrc={spot.img_src || undefined} // 이미지가 없으면 undefined를 전달하여 기본값을 사용하게 함
-            text={spot.name || spot.title || '야경 명소'} // 이름이나 제목을 표시, 없으면 기본값 사용
-          />
+        {spots.map((spot, _) => (
+          <ItemCard key={spot.NUM} imgSrc={spot.img_src || undefined} text={spot.TITLE || ' '} />
         ))}
       </div>
     </div>
