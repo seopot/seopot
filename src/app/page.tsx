2f8ccde0597baf7b55d8e9a25b7d5ea5fea0a500
Ju-MINJAE <juminjae98@gmail.com>
2025-04-15 21:25:39 +0900

    [Chore] use Arrow function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 'use client';
 
-import Input from '@/components/common/Input';
 import ItemCard from '@/components/common/ItemCard';
 import Modal from '@/components/common/Modal';
 import ModalContent from '@/components/ModalContent';
 import { useState } from 'react';
 
-export default function Home() {
+const Home = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const openModal = () => setIsModalOpen(true);
@@ -14,13 +13,12 @@ export default function Home() {
 
   return (
     <div className="flex flex-col justify-center items-center w-full ">
-      <Input />
       <div className="w-full flex flex-col sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4 md:gap-[2.125rem] px-4 md:px-[8.375rem] mt-8">
         <button onClick={openModal}>
-          <ItemCard />
+          <ItemCard text="example" />
         </button>
         <button onClick={openModal}>
-          <ItemCard />
+          <ItemCard text="example" />
         </button>
       </div>
       <Modal isOpen={isModalOpen} onClose={closeModal}>
@@ -28,4 +26,6 @@ export default function Home() {
       </Modal>
     </div>
   );
-}
+};
+
+export default Home;
