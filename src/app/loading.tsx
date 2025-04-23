5a9645d6e794f045fe1b21ffabfe998b78a629ec
Kwonyeojiny <kangagi0009@naver.com>
2025-04-23 23:28:11 +0900

    [Design] 로딩스피너 컴포넌트 mt 추가

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@ import LoadingSpinner from '@/components/common/LoadingSpinner';
 
 const Loading = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center mt-20">
       <LoadingSpinner />
     </div>
   );
