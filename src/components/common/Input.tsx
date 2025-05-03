15c670a3c070fd12cda90c3bf61af33978e202a4
Ju-MINJAE <juminjae98@gmail.com>
2025-05-03 13:35:43 +0900

    [Fix] remove unwanted border-radius between input and button

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -19,14 +19,14 @@ const Input = ({ onSearch }: { onSearch: (query: string) => void }) => {
         type="text"
         ref={inputRef}
         placeholder={tc('searchInput')}
-        className="w-full px-4 py-2 bg-lightBeige rounded-l-2xl focus:outline-none text-sm sm:text-base dark:text-navy"
+        className="w-full px-4 py-2 bg-lightBeige rounded-l-2xl rounded-r-none focus:outline-none text-sm sm:text-base dark:text-navy"
         onKeyDown={e => {
           if (e.key === 'Enter') handleSearch();
         }}
       />
       <button
         onClick={handleSearch}
-        className="flex justify-center items-center px-3 py-2 bg-lightBeige rounded-r-2xl hover:bg-lightBrown sm:px-4 dark:text-navy dark:hover:bg-lightNavy dark:hover:text-white"
+        className="flex justify-center items-center px-3 py-2 bg-lightBeige rounded-r-2xl rounded-l-none hover:bg-lightBrown sm:px-4 dark:text-navy dark:hover:bg-lightNavy dark:hover:text-white"
       >
         <Search className="w-4 h-4 sm:w-5 sm:h-5" />
       </button>
