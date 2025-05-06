1288d55cfcdcc8a058ee919335170e34934eeae2
Ju-MINJAE <juminjae98@gmail.com>
2025-05-06 17:28:42 +0900

    [Design] apply resposive design Lottie Component

--- src/components/HomeClient.tsx
+++ src/components/HomeClient.tsx
@@ -12,7 +12,7 @@ const Home = () => {
           서울의 야경, 유적지, 전통시장을 한눈에! 여행자를 위한 서울 명소 안내 서비스
         </p>
       </div>
-      <div className="absolute bottom-50% lg:right-0 w-[300px] h-[300px] z-0 lg:rotate-90">
+      <div className="absolute bottom-10 lg:bottom-52 lg:right-0 w-[300px] h-[300px] z-0 lg:rotate-90">
         <Lottie loop animationData={lottieData} play className="lg:rotate-180" />
       </div>
     </main>
